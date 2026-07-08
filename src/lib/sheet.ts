281acc69ef0c3f1dabddc8611846e5fa128d8442
Your <you@example.com>
2026-07-08 10:10:58 +0900

    fix: Change google.auth.JWT instantiation to object format to fix build type error

--- src/lib/sheet.ts
+++ src/lib/sheet.ts
@@ -137,12 +137,11 @@ async function fetchViaSheetsAPI(): Promise<Product[] | null> {
       credentials = JSON.parse(decoded);
     }
 
-    const auth = new google.auth.JWT(
-      credentials.client_email,
-      undefined,
-      credentials.private_key,
-      ['https://www.googleapis.com/auth/spreadsheets.readonly']
-    );
+    const auth = new google.auth.JWT({
+      email: credentials.client_email,
+      key: credentials.private_key,
+      scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly']
+    });
 
     const sheets = google.sheets({ version: 'v4', auth });
     
